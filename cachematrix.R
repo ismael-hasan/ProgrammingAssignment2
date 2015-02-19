ce8a47eec14c4c2c10f83f36a14469e56b2abfd2
ismael <ismael.hasan.romero@gmail.com>
2015-02-19 21:43:52 +0100

    Implementation of Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
+## Creates a matrix with a caching mechanism for its inverse.
+##
+## Args: 
+##   x: an invertible matrix. WARNING: only invertible matrices should be created. 
+## 
+## Returns: 
+##   A matrix with methods to obtain the cached inverse and to set it, and to get the original matrix and set it
+##
+## Error handling:
+##   None
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverted.matrix <- NULL
+  Set <- function(y) {
+    x <<- y
+    inverted.matrix <<- NULL
+  }
+  Get <- function() {
+    return(x)
+  }
+  SetInvertedMatrix <- function(inverted.matrix.input) {
+    inverted.matrix <<- inverted.matrix.input
+  }
+  GetInvertedMatrix <- function() {
+    return(inverted.matrix)
+  }
+  list(Set = Set, Get = Get,
+       SetInvertedMatrix = SetInvertedMatrix,
+       GetInvertedMatrix = GetInvertedMatrix)
 }
 
 
-## Write a short comment describing this function
-
+## Sets the inverse matrix for a cacheable matrix built with 'makeCacheMatrix' function
+##
+## Args:
+##   x: the cacheable matrix
+## 
+## Returns:
+##   The inverted matrix
+##
+## Error handling:
+##   None
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverted.matrix <- x$GetInvertedMatrix()
+  if(!is.null(inverted.matrix)) {
+    message("Getting cached data")
+    return(inverted.matrix)
+  } else {
+    message("Computing and caching matrix")
+    original.matrix <- x$Get()
+    inverted.matrix <- solve(original.matrix)
+    x$SetInvertedMatrix(inverted.matrix)
+    return(inverted.matrix)
+  }
 }
